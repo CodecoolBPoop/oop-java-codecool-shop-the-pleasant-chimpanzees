52600e4faf2c5d694bb23285c7284d58f0ce2da8
Miklós Balázs <bazsi2382@gmail.com>
2019-02-13 15:58:04 +0100

    RegisterController basics

---
+++ src/main/java/com/codecool/shop/controller/RegisterController.java
@@ -0,0 +1,31 @@
+package com.codecool.shop.controller;
+
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.UserDao;
+import com.sun.net.httpserver.HttpServer;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/register"})
+public class RegisterController extends HttpServlet {
+
+    //UserDao userDao = UserDao.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        engine.process("/product/cart.html", context, resp.getWriter());
+    }
+}
