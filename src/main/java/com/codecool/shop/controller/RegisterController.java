0af424694ddc1ae23ec15aad814411fdd6b44940
Miklós Balázs <bazsi2382@gmail.com>
2019-02-14 14:43:52 +0100

    registration is working without checking emails in database

--- src/main/java/com/codecool/shop/controller/RegisterController.java
+++ src/main/java/com/codecool/shop/controller/RegisterController.java
@@ -3,6 +3,7 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.dao.implementation.database.UserDaoJdbc;
 import com.codecool.shop.model.User;
 import com.sun.net.httpserver.HttpServer;
 import org.mindrot.jbcrypt.BCrypt;
@@ -31,16 +32,19 @@ public class RegisterController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Let's get the input secretly.
+
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        UserDao userDataStore = UserDaoJdbc.getInstance();
+
+        System.out.println("megerkezett");
+
         String email = req.getParameter("email");
         String password = req.getParameter("password");
-        String repassword = req.getParameter("repassword");
         String hashedPw = BCrypt.hashpw(password, BCrypt.gensalt());
-        String hashedRpw = BCrypt.hashpw(repassword, BCrypt.gensalt());
-        User user = new User(email, hashedPw);
 
-
-        System.out.printf("email: %s %npassword: %s %nhashedpw: %s %nrepassword: %s %nhashedRpw: %s", email, password, hashedPw, repassword, hashedRpw);
-
+        User user = new User(email, password);
+        userDataStore.add(user);
+        resp.sendRedirect("product/index.html");
     }
 }
