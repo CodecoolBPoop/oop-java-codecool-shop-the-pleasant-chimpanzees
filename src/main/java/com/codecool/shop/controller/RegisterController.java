7c4e6a5473323e4f5495b621ba63b3322fa7b06f
Miklós Balázs <bazsi2382@gmail.com>
2019-02-14 13:16:54 +0100

    register get method, post method basics

--- src/main/java/com/codecool/shop/controller/RegisterController.java
+++ src/main/java/com/codecool/shop/controller/RegisterController.java
@@ -3,7 +3,9 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.model.User;
 import com.sun.net.httpserver.HttpServer;
+import org.mindrot.jbcrypt.BCrypt;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -18,14 +20,27 @@ import java.io.IOException;
 @WebServlet(urlPatterns = {"/register"})
 public class RegisterController extends HttpServlet {
 
-    //UserDao userDao = UserDao.getInstance();
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
-        engine.process("/product/cart.html", context, resp.getWriter());
+        engine.process("/product/register.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Let's get the input secretly.
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String repassword = req.getParameter("repassword");
+        String hashedPw = BCrypt.hashpw(password, BCrypt.gensalt());
+        String hashedRpw = BCrypt.hashpw(repassword, BCrypt.gensalt());
+        User user = new User(email, hashedPw);
+
+
+        System.out.printf("email: %s %npassword: %s %nhashedpw: %s %nrepassword: %s %nhashedRpw: %s", email, password, hashedPw, repassword, hashedRpw);
+
     }
 }
